9a55760201a6e3792fc435dfd5852ff102bf28f3
Ivan Dario Sanchez Fonseca <ivancaviedes@hotmail.com>
2017-09-14 20:11:43 -0500

    Agrego servicio fake de libros

---
+++ FakeBooks/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FakeBooks.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ FakeBooks/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FakeBooks.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
